7885eb04ca7b1a04e4f963055390d8eab6db04a8
Trevor Ellermann <trevor@ellermann.net>
2014-10-29 19:08:08 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-galias
+lias
 ======
 
 Command line tool to create google groups as mail aliases for google apps
+
+
+## Requires gdata
+sudo pip install -U gdata
+
+## Usage
+
+Usage: galias.py [options] COMMAND 
+        
+Possible COMANDS are:         
+    listall - List all aliases         
+    list <alias> - list the specified alias         
+    add <alias> <destination> - add the <destination> to the <alias>         
+    delete <alias> <destination> - delete the <destination> from the <alias>         
+
+Options:
+  -h, --help            show this help message and exit
+  -u USERNAME, --username=USERNAME
+  -p PASSWORD, --password=PASSWORD
+  -d DOMAIN, --domain=DOMAIN
+  
+## Config
+Three pieces of informaiton are required, username, password and domain. If they are not given the program will prompt them. For faster use you can pass them on the command line with the above options or copy example_galias.ini to galias.ini and edit it with your information.
+
+## Examples
+#### List all aliases
+./galias.py listall
+
+#### List specific alias
+./galias.py list name@domain.com
+
+#### Add new alias to forward name@domain.com to other@email.com
+./galias.py *add* name@domain.com other@gmail.com
+
+#### Delete other@email.com from the name@domain.com alias
+./galias.py *delete* name@domain.com other@gmail.com
+
---
+++ galias.py
@@ -0,0 +1,146 @@
+#! /usr/bin/python
+import gdata.apps.groups.service
+
+def print_all_members(group_service):
+    groups = group_service.RetrieveAllGroups()
+    for group in groups:
+        print_group(group_service, group)
+
+def list_group(group_service, name):
+    group = group_service.RetrieveGroup(name)
+    print_group(group_service, group)
+
+def print_members(group_service, group_id):
+    gid = ""
+    for user in group_service.RetrieveAllMembers(group_id): 
+        print gid + "->", user['memberId']
+        gid = group_id + " "
+
+def add_to_alias(group_service, alias, address):
+    try:
+        group = group_service.RetrieveGroup(alias)
+    except Exception, e:
+        if e.reason == "EntityDoesNotExist":
+            print "New Alias " + alias
+            name = "Alias " + alias
+            group_service.CreateGroup(alias, name, "", "Anyone")
+            group = group_service.RetrieveGroup(alias)
+        else:
+            raise e
+
+    group_service.AddMemberToGroup(address, alias)
+    print "Added"
+    print "Current status of alias"
+    print_group(group_service, group)    
+    
+def delete_from_alias(group_service, alias, address):
+    try:
+        group = group_service.RetrieveGroup(alias)
+    except Exception, e:
+        if e.reason == "EntityDoesNotExist":
+            print "Invalid Alias " + alias
+        else:
+            raise e
+        return
+    
+    members = group_service.RetrieveAllMembers(alias)
+    memberlist = []
+    for member in members:
+        memberlist.append(member["memberId"])
+    if address not in memberlist:
+        print "*" * 70
+        print "* " + address + " is not in " + alias
+        print "*" * 70
+    else:
+        group_service.RemoveMemberFromGroup(address, alias)
+        print "Deleted"
+
+    members = group_service.RetrieveAllMembers(alias)
+    if not members:
+        group_service.DeleteGroup(alias)
+        print "Alias empty, removing alias"
+    else:
+        print "Current status of alias"
+        print_group(group_service, group)
+    
+def print_group(group_service, group): 
+    gid = group['groupId'] 
+    print('%s' % (gid)), 
+    print_members(group_service, gid) 
+
+def main():
+    from optparse import OptionParser
+    from optparse import OptionGroup
+    import os.path
+    import ConfigParser
+    config_username = ""
+    config_password = ""
+    config_domain = ""
+    if os.path.isfile("galias.ini"):
+        Config = ConfigParser.ConfigParser()
+        Config.read("galias.ini")
+        config_username = Config.get("galias", "username")
+        config_password = Config.get("galias", "password")
+        config_domain = Config.get("galias", "domain")
+    
+    usage = "usage: %prog [options] COMMAND \n\
+        \nPossible COMANDS are: \
+        \n    listall - List all aliases \
+        \n    list <alias> - list the specified alias \
+        \n    add <alias> <destination> - add the <destination> to the <alias> \
+        \n    delete <alias> <destination> - delete the <destination> from the <alias> \
+        "
+    parser = OptionParser(usage)
+
+    parser.add_option('-u', '--username', default=config_username)
+    parser.add_option('-p', '--password', default=config_password)
+    parser.add_option('-d', '--domain', default=config_domain)
+    group = OptionGroup(parser, "Dangerous Options",
+                    "Caution: use these options at your own risk.  "
+                    "It is believed that some of them bite.")
+
+    options, args = parser.parse_args()
+    command = ""
+
+    if len(args) < 1:
+        parser.error("incorrect number of arguments")
+    else:
+        command = args[0]
+
+    if not options.domain:
+        options.domain = raw_input("Google apps domain name: ")
+        
+    if not options.username:
+        username = raw_input("Your administrator username: ")
+        options.email = username + "@" + options.domain
+    else:
+        options.email = options.username + "@" + options.domain
+
+    if not options.password:       
+        import getpass 
+        password = getpass.getpass('Password: ')
+    else: 
+        password = options.password or login
+
+
+
+    group_service = gdata.apps.groups.service.GroupsService(email=options.email, domain=options.domain, password=password) 
+    group_service.ProgrammaticLogin()
+    
+    # COMMANDS
+    if command == "listall":
+        print_all_members(group_service)
+    elif command == "list":
+        print "listing alias", args[1]
+        list_group(group_service, args[1])
+    elif command == "add":
+        print "%s add %s" % (args[1], args[2])
+        add_to_alias(group_service, args[1], args[2])
+    elif command == "delete":
+        print "%s delete %s" % (args[1], args[2])
+        delete_from_alias(group_service, args[1], args[2])
+    else:
+        print "Unknown command"
+
+if __name__ == '__main__': 
+    main()
